777f1e3c6d6550e0f9dfbd1c7fe8ca760c95db6c
Ashutosh Pal <Ashutoshpal3179@gmail.com>
2026-06-09 10:02:23 +0530

    category table build successfuly

--- BulkyBookWeb/obj/Debug/net8.0/BulkyBookWeb.AssemblyInfo.cs
+++ BulkyBookWeb/obj/Debug/net8.0/BulkyBookWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21aae071c298bbe5a4538dfda2a29f5a6dd813ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
