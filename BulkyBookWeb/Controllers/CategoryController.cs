c9516c5115cdd942116fe091cb7b8b7ce52ed740
Ashutosh Pal <Ashutoshpal3179@gmail.com>
2026-06-11 20:41:59 +0530

    All feature implement successfully

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -14,8 +14,106 @@ namespace BulkyBookWeb.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            IEnumerable<Category> categoriesList = await _db.Categories.ToListAsync();
+            IEnumerable<Category> categoriesList = await _db.Categories.OrderByDescending(o=>o.CreatedDateTime).ToListAsync();
             return View(categoriesList);
         }
+
+        //Get
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the name.");
+            }
+            if (ModelState.IsValid)
+            {
+                await _db.Categories.AddAsync(obj);
+                await _db.SaveChangesAsync();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+        //Get
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || id==0) 
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = await _db.Categories.FindAsync(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                await _db.SaveChangesAsync();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+        //Get
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = await _db.Categories.FindAsync(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //Post
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeletePost(int? id)
+        {
+           var isCategoryExist=await _db.Categories.FindAsync(id);
+            if (isCategoryExist == null) {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(isCategoryExist);
+            await _db.SaveChangesAsync();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
--- BulkyBookWeb/obj/Debug/net8.0/BulkyBookWeb.AssemblyInfo.cs
+++ BulkyBookWeb/obj/Debug/net8.0/BulkyBookWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6362fec696e544b461a5e394d3318d470db8c8c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d30b67e301fa37dbe2b7a6ecfb774aec3250a9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
