5863ad9da5286e4a74d56b489b0a28055d7135a6
Ashutosh Pal <Ashutoshpal3179@gmail.com>
2026-06-15 06:45:32 +0530

    cache add

--- BulkyBookWeb/obj/Debug/net8.0/BulkyBookWeb.AssemblyInfo.cs
+++ BulkyBookWeb/obj/Debug/net8.0/BulkyBookWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9516c5115cdd942116fe091cb7b8b7ce52ed740")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95d0bc20009d0daa6bb0c75a3968d2610a6e910b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
